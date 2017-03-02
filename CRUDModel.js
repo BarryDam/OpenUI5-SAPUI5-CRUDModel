b70e525400b86c95426325795234c269e3b8364d
Barry Dam <barry.dam@newitera.nl>
2017-03-02 15:18:04 +0100

    auto login after page refresh

--- CRUDModel.js
+++ CRUDModel.js
@@ -1,7 +1,7 @@
 /**
  * nl.barrydam.model.CRUDModel
  * @author	Barry Dam
- * @version 1.2.6
+ * @version 1.3.0
  * add this file to your project folder library/bd/model/
  * In your Component.js add:
  * jQuery.sap.registerModulePath("nl.barrydam", "library/bd/");
@@ -9,6 +9,7 @@
  */
 (function($, windows, undefined){
 	jQuery.sap.declare('nl.barrydam.model.CRUDModel');
+	jQuery.sap.require("jquery.sap.storage");
 	sap.ui.define(
 		'nl/barrydam/model/CRUDModel',
 		['sap/ui/model/json/JSONModel'],
@@ -42,6 +43,85 @@
 				},
 				_methods = {}; // internal methods which can be used inside the CRUDModel methods
 			
+			// _base64 enc
+			var _base64 = {
+				_keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+				encode: function(input) {
+					var output = "", chr1, chr2, chr3, enc1, enc2, enc3, enc4, i = 0;
+					input = _base64._utf8_encode(String(input));
+					while (i < input.length) {
+						chr1 = input.charCodeAt(i++);
+						chr2 = input.charCodeAt(i++);
+						chr3 = input.charCodeAt(i++);
+						enc1 = chr1 >> 2;
+						enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+						enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+						enc4 = chr3 & 63;
+						if (isNaN(chr2)) {
+							enc3 = enc4 = 64;
+						} else if (isNaN(chr3)) {
+							enc4 = 64;
+						}
+						output = output + this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) + this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4);
+					}
+					return output;
+				},
+				decode: function(input) {
+					var output = "", chr1, chr2, chr3, enc1, enc2, enc3, enc4, i = 0;
+					input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+					while (i < input.length) {
+						enc1 = this._keyStr.indexOf(input.charAt(i++));
+						enc2 = this._keyStr.indexOf(input.charAt(i++));
+						enc3 = this._keyStr.indexOf(input.charAt(i++));
+						enc4 = this._keyStr.indexOf(input.charAt(i++));
+						chr1 = (enc1 << 2) | (enc2 >> 4);
+						chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+						chr3 = ((enc3 & 3) << 6) | enc4;
+						output = output + String.fromCharCode(chr1);
+						if (enc3 != 64) { output = output + String.fromCharCode(chr2); }
+						if (enc4 != 64) { output = output + String.fromCharCode(chr3); }
+					}
+					return _base64._utf8_decode(output);
+				},
+				_utf8_encode: function(string) {
+					string = string.replace(/\r\n/g, "\n");
+					var utftext = "";
+					for (var n = 0; n < string.length; n++) {
+						var c = string.charCodeAt(n);
+						if (c < 128) {
+							utftext += String.fromCharCode(c);
+						} else if ((c > 127) && (c < 2048)) {
+							utftext += String.fromCharCode((c >> 6) | 192);
+							utftext += String.fromCharCode((c & 63) | 128);
+						} else {
+							utftext += String.fromCharCode((c >> 12) | 224);
+							utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+							utftext += String.fromCharCode((c & 63) | 128);
+						}
+					}
+					return utftext;
+				},
+				_utf8_decode: function(utftext) {
+					var string = "", i = 0, c = 0, c1 = 0, c2 = 0;
+					while (i < utftext.length) {
+						c = utftext.charCodeAt(i);
+						if (c < 128) {
+							string += String.fromCharCode(c);
+							i++;
+						} else if ((c > 191) && (c < 224)) {
+							c2 = utftext.charCodeAt(i + 1);
+							string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+							i += 2;
+						} else {
+							c2 = utftext.charCodeAt(i + 1);
+							c3 = utftext.charCodeAt(i + 2);
+							string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+							i += 3;
+						}
+					}
+					return string;
+				}
+			};
 			
 			/**
 			 * magic _set methods *used in constructor and createSettersAndGetters,
@@ -236,6 +316,27 @@
 						}
 					}
 				);	
+			};
+
+			_methods.localStorageSaveCSRF = function(csrf) {
+				if (! jQuery.sap.storage.isSupported())
+					return;
+				var oStorage = jQuery.sap.storage(jQuery.sap.storage.Type.local);
+				if (csrf) {
+					oStorage.put('csrf', _base64.encode(csrf));
+				} else {
+					oStorage.remove('csrf');
+				}
+			};
+			_methods.localStorageGetCSRF = function() {
+				if (! jQuery.sap.storage.isSupported())
+					return;
+				var oStorage = jQuery.sap.storage(jQuery.sap.storage.Type.local),
+					csrf	= oStorage.get("csrf");
+				if (! csrf) {
+					return false;
+				}
+				return _base64.decode(csrf);
 			};
 
 
@@ -602,13 +703,20 @@
 						}
 						// merge the mDefaultSettings with the _mSettings to make sure whe have every needed param
 						this._mSettings	= $.extend(true, {}, _variables.mDefaultParameters, this._mSettings);
+						// try to set the csrf from local storage
+						// so if your logged in the api, you do not need to re enter your credentials everytime you reload the page
+						var sCsrfLocalStorage = _methods.localStorageGetCSRF();
+						_variables.csrf = sCsrfLocalStorage;	
 						// metadata
 						var that			= this, 
-							fnLoadMetadata	= function() {
+							fnLoadMetadata	= function(fnOnsuccess) {
 								that._serviceCall("", {
 									success: function(m) {
 										_methods.processMetadata(that, m);
 										that.fireMetadataLoaded();
+										if (typeof fnOnsuccess == "function") {
+											fnOnsuccess();
+										}
 									},
 									error: function(xhr, textStatus, httpStatus) {
 										that.fireMetadataFailed();	
@@ -619,9 +727,10 @@
 											});
 										}
 									},
-									async: false // keep this false
+									async: (that.getUser() && that.getPassword()) ? false : ((sCsrfLocalStorage) ?  true : false) 
 								});
-							};		
+							};
+						
 						// user settings passed in constructor
 						if (this.getUser() && this.getPassword()) {
 							this.login(
@@ -632,7 +741,11 @@
 								}
 							);
 						} 
-						fnLoadMetadata();	
+						fnLoadMetadata(function onSuccess(){
+							if (sCsrfLocalStorage) { // if this has an value, the api uses user credentials
+								that.fireLogin();
+							}
+						});	
 									
 						
 					}
@@ -741,8 +854,7 @@
 							});
 							// fire logout
 							// IMPORTANT: allways put this AFTER above sync abortions
-							that.fireLogout();
-							
+							that.fireLogout();							
 						} 
 						mRequestParams.error.apply(this, arguments);
 					},
@@ -1027,6 +1139,7 @@
 					},
 					success		: function(sCSRF) {
 						_variables.csrf = sCSRF;
+						_methods.localStorageSaveCSRF(sCSRF);
 						that.fireLogin();
 						if (typeof mParameters.success == "function") {
 							mParameters.success();
